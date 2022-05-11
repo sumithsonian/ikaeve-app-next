01d7f31dffdb5d6486a91c81249852bf4246e65d
角雄崇 <sumi@ini.co.jp>
2022-05-11 14:41:27 +0900

    update: Default状態に戻した

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,5 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
-declare module '*.scss';  // .scssファイルのimportを構文エラーにしない
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
